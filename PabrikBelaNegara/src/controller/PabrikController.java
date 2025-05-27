4eb7bf20c1c25aad3c81806117a77a0336fccf39
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-05-27 12:32:17 +0700

    NO comment

--- PabrikBelaNegara/src/controller/PabrikController.java
+++ PabrikBelaNegara/src/controller/PabrikController.java
@@ -53,8 +53,11 @@ public class PabrikController {
         p.setJamKerja(Integer.parseInt(frame.getjTxtJamKerja().getText().trim()));
         p.setJumlahTenagaKerja(Integer.parseInt(frame.getjTxtJumlahTenagaKerja().getText().trim()));
         p.setBiayaBahanBaku(Integer.parseInt(frame.getjTxtBiayaBahanBaku().getText().trim()));
-        p.setBiayaTenagaKerja(p.getJamKerja() * p.getJumlahTenagaKerja() * 1500);
+        p.setBiayaTenagaKerja(p.getJamKerja() * p.getJumlahTenagaKerja() * 15000);
         p.setEfisiensiProduksi((p.getJumlahUnit() / p.getJamKerja() * p.getJumlahTenagaKerja()) * 100);
+        if (p.getEfisiensiProduksi() < 20) {
+            p.setEfisiensiProduksi(0 - 20);
+        }
         p.setTotalBiayaProduksi(p.getBiayaBahanBaku() + p.getBiayaTenagaKerja());
         p.setId(selectedId);
         impl.update(p);
